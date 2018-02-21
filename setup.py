ca7a380369aa1674afef51d40168380c8607a3fa
github kyleyang <kxy156@case.edu>
2018-02-20 20:56:40 -0700

    setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='githubcountpy',
+setup(name='GithubCountPy',
       version='0.0.0',
       description='No user, password and access token needed.',
       url='https://github.com/keyanyang/githubcountpy',
       author='Kyle Yang',
       author_email='keyanyoung@gmail.com',
       license='MIT',
-      packages=['GithubCountPy', 'GithubCountPy.GithubCountPy'],
+      packages=['GithubCountPy'],
       install_requires=[
           'beautifulsoup4==4.6.0', 'requests==2.18.4'
       ],
